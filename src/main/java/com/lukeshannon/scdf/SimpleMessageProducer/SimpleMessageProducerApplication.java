d054092e8ba7dba01108fbc96b22b1cb3346b947
Luke Shannon <luke.shannon@gmail.com>
2018-06-19 17:38:38 -0400

    Update SimpleMessageProducerApplication.java

--- src/main/java/com/lukeshannon/scdf/SimpleMessageProducer/SimpleMessageProducerApplication.java
+++ src/main/java/com/lukeshannon/scdf/SimpleMessageProducer/SimpleMessageProducerApplication.java
@@ -1,11 +1,10 @@
 package com.lukeshannon.scdf.SimpleMessageProducer;
 
+import java.util.Date;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,10 +12,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.task.configuration.EnableTask;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpEntity;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
 import org.springframework.web.client.RestTemplate;
 
 @EnableTask
 @SpringBootApplication
+@EnableJms
 public class SimpleMessageProducerApplication {
 	
 	private static final Logger log = LoggerFactory.getLogger(SimpleMessageProducerApplication.class);
@@ -29,51 +34,74 @@ public class SimpleMessageProducerApplication {
 	public CommandLineRunner commandLineRunner() {
 		return new SimpleMessageRunner();
 	}
+	
+
 
 	public static class SimpleMessageRunner implements CommandLineRunner {
 
 		/*
 		 * To get the code to compile a valid looking URL needs be set
 		 */
-		@Value("${endpoint}")
-		@NotNull
-		@Size(min=1)
+		@Value("${endpoint:null}")
 		private String endpoint;
 		
-		@Value("${message}")
-		@NotNull
-		@Size(min=1)
+		@Value("${message:null}")
 		private String message;
 		
 		@Value("${numberOfMessages}")
 		@Min(1)
 		private int numberOfMessages;
 		
-		@Value("${complexMessage}")
-		private boolean complexMessage;
+		@Value("${pojo:false}")
+		private boolean pojo;
+		
+		@Value("${useJms:false}")
+		private boolean useJms;
+		
+		@Value("${jmsDestination:null}")
+		private String jmsDestination;
+		
+		@Autowired
+		private JmsTemplate jmsTemplate;
+		
+		@Bean // Serialize message content to json using TextMessage
+	    public MessageConverter jacksonJmsMessageConverter() {
+	        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+	        converter.setTargetType(MessageType.TEXT);
+	        converter.setTypeIdPropertyName("_type");
+	        return converter;
+	    }
 
 		@Override
 		public void run(String... strings) throws Exception {
-			RestTemplate restTemplate = new RestTemplate();
-			log.info("Ready to send messges to : " + endpoint);
-			if (endpoint != null) {
+				RestTemplate restTemplate = new RestTemplate();
+				log.info("Sending " + numberOfMessages + " Using JMS: " + useJms + " Pojo Messages: " + pojo + " Time: " + new Date());
 				for (int i = 0; i < numberOfMessages; i++) {
-					if (complexMessage) {
-						HttpEntity<SimpleMessage> payload = new HttpEntity<SimpleMessage>(new SimpleMessage("Simple Message: " + i, i, message));
-						restTemplate.postForLocation(endpoint, payload);
-						log.info("Sent message: " +  payload.getBody() + " to " + endpoint);
+					if (pojo) {
+						SimpleMessage simpleMessage =  new SimpleMessage("Simple Message: " + i, i, message);
+						if (useJms) {
+							jmsTemplate.convertAndSend(jmsDestination, simpleMessage);
+							log.info("Sent message: " +  simpleMessage + " to destination " + jmsDestination);
+						} else {
+							HttpEntity<SimpleMessage> payload = new HttpEntity<SimpleMessage>(simpleMessage);
+							restTemplate.postForLocation(endpoint, payload);
+							log.info("Sent message: " +  payload.getBody() + " to " + endpoint);
+						}
+					} else {
+						String messageToSend = message + " : " + i;
+						if (useJms) {
+							jmsTemplate.convertAndSend(jmsDestination, messageToSend);
+							log.info("Sent message: " +  messageToSend + " to destination " + jmsDestination);
+						} else {
+							HttpEntity<String> payload = new HttpEntity<String>(message + " : " + i);
+							restTemplate.postForLocation(endpoint, payload);
+							log.info("Sent message: " +  payload.getBody() + " to " + endpoint);
+						}
 					}
-					else {
-						HttpEntity<String> payload = new HttpEntity<String>(message + " : " + i);
-						restTemplate.postForLocation(endpoint, payload);
-						log.info("Sent message: " +  payload.getBody() + " to " + endpoint);
-					}
+					
 				}
-			}
-			else {
-				log.error("Endpoint value is not set. No messages sent");
-			}
 		}
+		
 		
 		class SimpleMessage {
 			
